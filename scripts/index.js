9e7f3a32d37a1a1a949dba60f2455a6ff1deb330
stalyn <smtihsly@gmail.com>
2023-12-09 19:47:54 -0500

    gitcard finish

--- scripts/index.js
+++ scripts/index.js
@@ -4,7 +4,7 @@ const giftbox = document.getElementById('merrywrap');
 const canvasC = document.getElementById('c');
 
 const config = {
-  birthdate: 'December 09, 2023',
+  birthdate: 'December 11, 2023',
   name: 'Luz Dari'
 };
 
